67d1dc96f1c2157ca907320fb37454e9bf91150d
benR <momentumdd@me.com>
2018-10-02 07:29:35 -0600

    Project 2
    Udates to project 1 javascript and added project 2

--- includes/js/library.js
+++ includes/js/library.js
@@ -1,11 +1,14 @@
 showPassword = function () {
-    // listening for form submissions
-    document.addEventListener('change', function (event) {
-        
+
+    var togglePassword = function (event) {
+
+        // See if the element is our checkbox if not quit and return
+        if (!event.target.matches('[data-toggle-password]')) return;
+
         var form = event.target.closest('form'),
             password = form.querySelectorAll('[data-password]');
-        
-        // if checkbox is checked change type to text else if unchecked change to password
+
+        // if input has type checkbox and is checked/unchecked
         if (event.target.checked == true) {
             for (let i = 0; i < password.length; i++) {
                 password[i].type = 'text';
@@ -15,6 +18,49 @@ showPassword = function () {
                 password[i].type = 'password';
             }
         }
+    };
 
-    }, false)
+    // listening for input field events
+    document.addEventListener('input', togglePassword, false);
 };
+
+temperatureConv = function () {
+
+    var convertTemp = function (event) {
+
+        // See if the element is our checkbox if not quit and return
+        if (!event.target.matches('[data-temperature]')) return;
+
+        var celcius = document.querySelector('#celcius'),
+            tempCelcius = celcius.value,
+            farenheit = document.querySelector('#farenheit'),
+            tempFarenheit = farenheit.value;
+        // 
+        if (event.target.id === 'farenheit') {
+            farenheitCelcius(tempFarenheit);
+        }
+
+        if (event.target.id === 'celcius') {
+            celciusFarenheit(tempCelcius);
+        }
+
+    };
+
+    var farenheitCelcius = function(temp) {
+        // convert from F to C
+        celcius.value = '';
+        convertTempC = (temp - 32) * 5 / 9;
+        celcius.value = Math.round(convertTempC);
+    };
+
+    var celciusFarenheit = function(temp) {
+        // convert from C to F
+        farenheit.value = ''
+        convertTempF = (temp * 9 / 5) + 32;
+        farenheit.value = Math.round(convertTempF);
+    };
+
+
+    document.documentElement.addEventListener('input', convertTemp, false);
+
+}
